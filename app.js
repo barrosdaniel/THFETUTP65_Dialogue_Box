ab7241a983117573f4e8e8639f0c450e03916634
Daniel Barros <barros.daniel@outlook.com>
2019-08-13 08:41:51 +0800

    feature: Add dialog box and activating buttons

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-(function() {
-   
-  // Code goes here...
-  
+(function () {
+  const openDialog = document.getElementById('openDialog');
+  const openModalDialog = document.getElementById('openModalDialog');
+  const dialogWindow = document.getElementById('dialogWindow');
+
+  // Open Dialog Window Normally
+  openDialog.addEventListener('click', () => {
+    dialogWindow.show();
+  });
+
+  // Open Dialog Window as a Modal
+  openModalDialog.addEventListener('click', () => {
+    dialogWindow.showModal();
+  });
+
 })();
